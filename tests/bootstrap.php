e5fa994f962b79aa691d9e863d0b393454703eea
Cixtor <cixtords@gmail.com>
2016-05-19 13:24:01 -0500

    Added code to download a fresh copy of the WordPress

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,6 +2,18 @@
 
 define('WP_TEST_DIR', __DIR__ . '/../wordpress');
 
+if (!file_exists(WP_TEST_DIR)) {
+    $output = array();
+    $return_var = 255;
+    $command = 'svn co https://develop.svn.wordpress.org/trunk/ wordpress';
+
+    printf("@ WordPress unit-test suite\n");
+    printf("@ Cloning development repository... ");
+    @exec($command, $output, $return_var);
+    printf("DONE\n");
+    exit(0);
+}
+
 require_once(WP_TEST_DIR . '/tests/phpunit/includes/functions.php');
 
 if (function_exists('tests_add_filter')) {
