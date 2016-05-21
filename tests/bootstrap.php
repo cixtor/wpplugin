bc4816e225c42873761b58efb6edbfe2c9afc64f
Cixtor <cixtords@gmail.com>
2016-05-21 13:25:23 -0500

    Added code to speed-up the execution time of phpunit

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,7 @@
 <?php
 
 define('WP_TEST_DIR', __DIR__ . '/../wordpress');
+define('WP_INSTALLER', WP_TEST_DIR . '/tests/phpunit/includes/install.php');
 
 if (!file_exists(WP_TEST_DIR)) {
     $output = array();
@@ -27,6 +28,37 @@ if (!file_exists(WP_TEST_DIR)) {
 
         printf("  Setting website hostname\n");
         $config = str_replace('example.org', 'wordpress.test', $config);
+
+        printf("@ Improve installation script\n");
+        $installer = @file(WP_INSTALLER, FILE_IGNORE_NEW_LINES);
+        $new_installer = array();
+
+        foreach ($installer as $line) {
+            if (strpos($line, 'Installing...') !== false) {
+                $new_installer[] = $line;
+                $new_installer[] = 'if (isset($_SERVER["WP_TRUNCATE_DB"])) {';
+                $new_installer[] = '$start_time = microtime(true);';
+                continue;
+            }
+
+            if (strpos($line, 'wp_install(') !== false) {
+                $new_installer[] = $line;
+                $new_installer[] = '$total = (microtime(true) - $start_time);';
+                $new_installer[] = 'printf("Execution time %s secs\n", $total);';
+                $new_installer[] = '} /* Skip database truncate */';
+                continue;
+            }
+
+            $new_installer[] = $line;
+        }
+
+        $new_installer_text = implode("\n", $new_installer) . "\n";
+        file_put_contents(WP_INSTALLER, $new_installer_text);
+        printf("  By default the database is truncated every time PHPUnit \n"
+            .  "  is executed. The installer script has been modified to \n"
+            .  "  speed up the execution time for simple test cases, you \n"
+            .  "  set the WP_TRUNCATE_DB=true environment variable to run \n"
+            .  "  the original behavior: WP_TRUNCATE_DB=true phpunit\n");
 
         file_put_contents('wordpress/wp-tests-config.php', $config);
         printf("@ Finished\n");
